91409aa21b2ea9201fa4bc81e13a7ffa39001e48
dumbrekka <fornewsletters20@gmail.com>
2025-11-14 21:08:15 +0000

    whatif

---
+++ storage.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+
+
+contract SimpleStorage {
+
+    uint256 public Number = 0;
+
+    struct Person {
+    uint favNumber;
+    string name;
+}
+
+Person public jamie = Person({favNumber: 13, name: "jamie"});
+
+    function store(uint _favNumber) public {
+        Number = _favNumber;
+    }
+    function retrieve() public view returns(uint256) {
+        return Number;
+    }
+}
