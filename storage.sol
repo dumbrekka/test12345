27ea06e1979df873a5e0953023fc31bbafe0fd31
dumbrekka <fornewsletters20@gmail.com>
2025-11-14 22:58:50 +0000

    poom

--- storage.sol
+++ storage.sol
@@ -3,23 +3,27 @@ pragma solidity 0.8.19;
 
 
 
-contract SimpleStorage {
+contract Storage {
 
     uint256 public Number = 0;
-    uint256 public Number1 = 0;
-    uint256 public Number2 = 0;
 
     struct Person {
     uint favNumber;
     string name;
-}
+    }
 
-Person public jamie = Person({favNumber: 13, name: "jamie"});
+    Person[] public listOfPeople;
+    mapping(string => uint) public nameTofavNumber;
 
     function store(uint _favNumber) public {
         Number = _favNumber;
     }
     function retrieve() public view returns(uint256) {
         return Number;
+    }
+
+    function addPerson(string memory _name, uint _favNumber) public {
+        listOfPeople.push(Person(_favNumber, _name));
+        nameTofavNumber[_name] = _favNumber;
     }
 }
